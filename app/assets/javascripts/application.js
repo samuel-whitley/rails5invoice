5d7dd977b7253a6fb00e40d429e6470ef049757c
samuel whitley <samuel.whitley89@gmail.com>
2016-09-01 13:06:54 -0400

    add total to orders/edit javascripts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,29 +20,63 @@
 
 
 $(document).on('turbolinks:load',function(){
-  
+
 
   function addField () {
     var time = new Date();
     var mSec = time.getTime();
 
-    idAttr = "order_products_attributes_0_name".replace("0",mSec)
-    nameAttr = "order[products_attributes][0][name]".replace("0",mSec)
+    var idAttrName = "order_products_attributes_0_name".replace("0",mSec)
+    var nameAttrName = "order[products_attributes][0][name]".replace("0",mSec)
 
-    var li = $("<li></li>")
-    var input = $("<input></input>")
+    var idAttrQuantity = "order_products_attributes_0_quantity".replace("0",mSec)
+    var nameAttrQuantity = "order[products_attributes][0][quantity]".replace("0",mSec)
 
-    input.attr({
+    var idAttrPrice = "order_products_attributes_0_price".replace("0",mSec)
+    var nameAttrPrice = "order[products_attributes][0][price]".replace("0",mSec)
+
+    var divQuanity = $("<div></div>");
+    var divPrice = $("<div></div>");
+    var divName = $("<div></div>");
+
+    var inputQuanity = $("<input></input>");
+    var inputPrice = $("<input></input>");
+    var inputName = $("<input></input>");
+
+    inputName.attr({
       type: "text",
-      id: idAttr,
-      name: nameAttr
+      id: idAttrName,
+      name: nameAttrName
     })
 
-    input.addClass("form-control");
+    inputQuanity.attr({
+      type: "text",
+      id: idAttrQuantity,
+      name: nameAttrQuantity
+    })
 
-    li.append(input);
-    var list = $("#nameList")
-    list.append(li);
+    inputPrice.attr({
+      type: "text",
+      id: idAttrPrice,
+      name: nameAttrPrice
+    })
+
+    inputName.addClass("form-control");
+    divQuanity.addClass("col-xs-4");
+    divPrice.addClass("col-xs-4");
+    divName.addClass("col-xs-4");
+
+    inputQuanity.addClass("form-control");
+    inputName.addClass("tags")
+    inputPrice.addClass("form-control");
+
+    divQuanity.append(inputQuanity);
+    divPrice.append(inputPrice);
+    divName.append(inputName);
+
+
+    var list = $("#product_list")
+    list.append(divQuanity,divPrice,divName);
 
   }
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831223734) do
+ActiveRecord::Schema.define(version: 20160831230929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160831223734) do
     t.boolean  "paid"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "total"
   end
 
   create_table "products", force: :cascade do |t|
